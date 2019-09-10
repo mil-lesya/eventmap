8c9b859818a4fbabfd07040e2e02d3772b51e69a
Mileshko Lesya <mileshko.lesya@gmail.com>
2019-09-10 19:13:27 +0300

    database

---
+++ src/main/resources/eventmap.sql
@@ -0,0 +1,48 @@
+create table "user"
+(
+    id      bigserial not null
+        constraint user_pk
+            primary key,
+    name    varchar   not null,
+    surname varchar   not null,
+    mail    varchar   not null,
+    hash    varchar   not null
+);
+
+alter table "user"
+    owner to postgres;
+
+create unique index user_mail_uindex
+    on "user" (mail);
+
+create table event
+(
+    id          bigserial not null
+        constraint event_pk
+            primary key,
+    date        timestamp not null,
+    description text,
+    price       integer,
+    name        varchar(20)
+);
+
+alter table event
+    owner to postgres;
+
+create table visited_event
+(
+    id         bigserial not null
+        constraint visited_event_pk
+            primary key,
+    event_id   bigint    not null
+        constraint visited_event_event_id_fk
+            references event,
+    user_id    bigint    not null
+        constraint visited_event_user_id_fk
+            references "user",
+    assessment boolean
+);
+
+alter table visited_event
+    owner to postgres;
+
