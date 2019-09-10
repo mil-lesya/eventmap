561f6dac405498ac760b21f1262a90f4c385a7aa
Mileshko Lesya <mileshko.lesya@gmail.com>
2019-09-10 19:08:33 +0300

    Initial commit

---
+++ src/main/java/com/gmail/mileshko/lesya/eventmap/EventmapApplication.java
@@ -0,0 +1,13 @@
+package com.gmail.mileshko.lesya.eventmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventmapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventmapApplication.class, args);
+    }
+
+}
