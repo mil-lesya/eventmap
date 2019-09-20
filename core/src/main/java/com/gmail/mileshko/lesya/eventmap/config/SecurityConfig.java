7ca6123a9bab9dc539bd257575d0955d8acdca7a
Mileshko Lesya <mileshko.lesya@gmail.com>
2019-09-20 13:05:51 +0300

    backend: added web configuration

--- core/src/main/java/com/gmail/mileshko/lesya/eventmap/config/SecurityConfig.java
+++ core/src/main/java/com/gmail/mileshko/lesya/eventmap/config/SecurityConfig.java
@@ -13,10 +13,10 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        http
+                .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/", "/login", "/register").permitAll()
                 .anyRequest().authenticated()
