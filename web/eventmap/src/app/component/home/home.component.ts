b3ed5afc46db6c524ededa94793d210fc9e434ed
Mileshko Lesya <mileshko.lesya@gmail.com>
2019-09-20 18:00:51 +0300

    frontend: map added

--- web/eventmap/src/app/component/home/home.component.ts
+++ web/eventmap/src/app/component/home/home.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
-  constructor() { }
 
-  ngOnInit() {
+  lat = 43.879078;
+  lng = -103.4615581;
+  selectedMarker;
+  markers = [];
+
+  addMarker(lat: number, lng: number) {
+    this.markers.push({lat, lng, alpha: 0.4}
+    );
   }
 
+  max(coordType: 'lat' | 'lng'): number {
+    return Math.max(...this.markers.map(marker => marker[coordType]));
+  }
+
+  min(coordType: 'lat' | 'lng'): number {
+    return Math.min(...this.markers.map(marker => marker[coordType]));
+  }
+
+  selectMarker(event) {
+    this.selectedMarker = {
+      lat: event.latitude,
+      lng: event.longitude
+    };
+  }
 }
+
+
